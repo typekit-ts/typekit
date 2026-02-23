86090ddbd8ba969dba3715e21b80eb7162ab8923
pekochan069 <pekochan069@gmail.com>
2026-02-23 23:53:04 +0900

    feat: create Either

---
+++ packages/typekit/src/either/either.test.ts
@@ -0,0 +1,351 @@
+import { describe, expect, test } from "bun:test";
+
+import { Either } from ".";
+
+describe("either", () => {
+  test("left()", () => {
+    const value = Either.left(1);
+
+    expect(value._tag).toBe("left");
+  });
+
+  test("right()", () => {
+    const value = Either.right("value");
+
+    expect(value._tag).toBe("right");
+  });
+
+  test("isLeft()", () => {
+    const leftValue = Either.left(1);
+    const value1 = Either.isLeft(leftValue);
+
+    expect(value1).toBe(true);
+
+    const rightValue = Either.right("value");
+    const value2 = Either.isLeft(rightValue);
+
+    expect(value2).toBe(false);
+  });
+
+  test("isRight()", () => {
+    const leftValue = Either.left(1);
+    const value1 = Either.isRight(leftValue);
+
+    expect(value1).toBe(false);
+
+    const rightValue = Either.right("value");
+    const value2 = Either.isRight(rightValue);
+
+    expect(value2).toBe(true);
+  });
+
+  test("unwrapLeft()", () => {
+    const leftValue = Either.left(1);
+    const rightValue = Either.right("value");
+
+    const value = Either.unwrapLeft(leftValue);
+
+    expect(value).toBe(1);
+
+    expect(() => Either.unwrapLeft(rightValue)).toThrow();
+  });
+
+  test("unwrapLeftOr()", () => {
+    const leftValue = Either.left(1);
+    const rightValue = Either.right("value");
+
+    const value1 = Either.unwrapLeftOr(leftValue, 2);
+    const value2 = Either.unwrapLeftOr(rightValue, 2);
+
+    expect(value1).toBe(1);
+    expect(value2).toBe(2);
+  });
+
+  test("unwrapRight()", () => {
+    const leftValue = Either.left(1);
+    const rightValue = Either.right("value");
+
+    const value = Either.unwrapRight(rightValue);
+
+    expect(value).toBe("value");
+
+    expect(() => Either.unwrapRight(leftValue)).toThrow();
+  });
+
+  test("unwrapRightOr()", () => {
+    const leftValue = Either.left(1);
+    const rightValue = Either.right("value");
+
+    const value1 = Either.unwrapRightOr(leftValue, "default");
+    const value2 = Either.unwrapRightOr(rightValue, "default");
+
+    expect(value1).toBe("default");
+    expect(value2).toBe("value");
+  });
+
+  test("flatten()", () => {
+    const leftValue = Either.left<number, Either.Either<number, string>>(1);
+    const rightWithRight = Either.right<number, Either.Either<number, string>>(Either.right("value"));
+    const rightWithLeft = Either.right<number, Either.Either<number, string>>(Either.left(1));
+
+    const flattenedLeft = Either.flatten(leftValue);
+    const flattenedRightWithRight = Either.flatten(rightWithRight);
+    const flattenedRightWithLeft = Either.flatten(rightWithLeft);
+
+    expect(flattenedLeft._tag).toBe("left");
+    if (Either.isLeft(flattenedLeft)) {
+      expect(flattenedLeft.left).toBe(1);
+    }
+
+    expect(flattenedRightWithRight._tag).toBe("right");
+    if (Either.isRight(flattenedRightWithRight)) {
+      expect(flattenedRightWithRight.right).toBe("value");
+    }
+
+    expect(flattenedRightWithLeft._tag).toBe("left");
+    if (Either.isLeft(flattenedRightWithLeft)) {
+      expect(flattenedRightWithLeft.left).toBe(1);
+    }
+  });
+
+  test("flatten2()", () => {
+    const nestedLeftValue = Either.left<Either.Either<number, string>, Either.Either<number, string>>(Either.left(1));
+    const nestedRightValue = Either.left<Either.Either<number, string>, Either.Either<number, string>>(
+      Either.right("value"),
+    );
+    const rightValue = Either.right<Either.Either<number, string>, Either.Either<number, string>>(
+      Either.right("value"),
+    );
+
+    const flattenedNestedLeft = Either.flatten2(nestedLeftValue);
+    const flattenedNestedRight = Either.flatten2(nestedRightValue);
+    const flattenedRight = Either.flatten2(rightValue);
+
+    expect(flattenedNestedLeft._tag).toBe("left");
+    if (Either.isLeft(flattenedNestedLeft)) {
+      expect(flattenedNestedLeft.left).toBe(1);
+    }
+
+    expect(flattenedNestedRight._tag).toBe("right");
+    if (Either.isRight(flattenedNestedRight)) {
+      expect(flattenedNestedRight.right).toBe("value");
+    }
+
+    expect(flattenedRight._tag).toBe("right");
+    if (Either.isRight(flattenedRight)) {
+      expect(flattenedRight.right).toBe("value");
+    }
+  });
+
+  test("mapLeft() - data first", () => {
+    const add1 = (value: number) => value + 1;
+    const leftValue = Either.left(1);
+    const rightValue = Either.right("value");
+
+    const mappedLeft = Either.mapLeft(leftValue, add1);
+    const mappedRight = Either.mapLeft(rightValue, add1);
+
+    expect(mappedLeft._tag).toBe("left");
+    if (Either.isLeft(mappedLeft)) {
+      expect(mappedLeft.left).toBe(2);
+    }
+    expect(mappedRight).toBe(rightValue);
+  });
+
+  test("mapLeft() - data last", () => {
+    const add1 = (value: number) => value + 1;
+    const leftValue = Either.left(1);
+    const rightValue = Either.right("value");
+
+    const mappedLeft = Either.mapLeft(add1)(leftValue);
+    const mappedRight = Either.mapLeft(add1)(rightValue);
+
+    expect(mappedLeft._tag).toBe("left");
+    if (Either.isLeft(mappedLeft)) {
+      expect(mappedLeft.left).toBe(2);
+    }
+    expect(mappedRight).toBe(rightValue);
+  });
+
+  test("mapRight() - data first", () => {
+    const addBang = (value: string) => `${value}!`;
+    const leftValue = Either.left(1);
+    const rightValue = Either.right("value");
+
+    const mappedLeft = Either.mapRight(leftValue, addBang);
+    const mappedRight = Either.mapRight(rightValue, addBang);
+
+    expect(mappedLeft).toBe(leftValue);
+    expect(mappedRight._tag).toBe("right");
+    if (Either.isRight(mappedRight)) {
+      expect(mappedRight.right).toBe("value!");
+    }
+  });
+
+  test("mapRight() - data last", () => {
+    const addBang = (value: string) => `${value}!`;
+    const leftValue = Either.left(1);
+    const rightValue = Either.right("value");
+
+    const mappedLeft = Either.mapRight(addBang)(leftValue);
+    const mappedRight = Either.mapRight(addBang)(rightValue);
+
+    expect(mappedLeft).toBe(leftValue);
+    expect(mappedRight._tag).toBe("right");
+    if (Either.isRight(mappedRight)) {
+      expect(mappedRight.right).toBe("value!");
+    }
+  });
+
+  test("biMap() - data first", () => {
+    const leftValue = Either.left(1);
+    const rightValue = Either.right("value");
+
+    const bimapFn = {
+      onLeft: (value: number) => value + 1,
+      onRight: (value: string) => `${value}!`,
+    };
+
+    const mappedLeft = Either.biMap(leftValue, bimapFn);
+    const mappedRight = Either.biMap(rightValue, bimapFn);
+
+    expect(mappedLeft._tag).toBe("left");
+    if (Either.isLeft(mappedLeft)) {
+      expect(mappedLeft.left).toBe(2);
+    }
+    expect(mappedRight._tag).toBe("right");
+    if (Either.isRight(mappedRight)) {
+      expect(mappedRight.right).toBe("value!");
+    }
+  });
+
+  test("biMap() - data last", () => {
+    const leftValue = Either.left(1);
+    const rightValue = Either.right("value");
+
+    const bimapFn = Either.biMap({
+      onLeft: (value: number) => value + 1,
+      onRight: (value: string) => `${value}!`,
+    });
+
+    const mappedLeft = bimapFn(leftValue);
+    const mappedRight = bimapFn(rightValue);
+
+    expect(mappedLeft._tag).toBe("left");
+    if (Either.isLeft(mappedLeft)) {
+      expect(mappedLeft.left).toBe(2);
+    }
+    expect(mappedRight._tag).toBe("right");
+    if (Either.isRight(mappedRight)) {
+      expect(mappedRight.right).toBe("value!");
+    }
+  });
+
+  test("flatMapLeft() - data first", () => {
+    const leftValue: Either.Either<number, string> = Either.left(2);
+    const rightValue: Either.Either<number, string> = Either.right("value");
+
+    const flatMapFn = (value: number) => Either.left<number, string>(value * 2);
+
+    const mappedLeft = Either.flatMapLeft(leftValue, flatMapFn);
+    const mappedRight = Either.flatMapLeft(rightValue, flatMapFn);
+
+    expect(mappedLeft._tag).toBe("left");
+    if (Either.isLeft(mappedLeft)) {
+      expect(mappedLeft.left).toBe(4);
+    }
+    expect(mappedRight).toBe(rightValue);
+  });
+
+  test("flatMapLeft() - data last", () => {
+    const leftValue: Either.Either<number, string> = Either.left(2);
+    const rightValue: Either.Either<number, string> = Either.right("value");
+
+    const flatMapFn = Either.flatMapLeft((value: number) => Either.left<number, string>(value * 2));
+
+    const mappedLeft = flatMapFn(leftValue);
+    const mappedRight = flatMapFn(rightValue);
+
+    expect(mappedLeft._tag).toBe("left");
+    if (Either.isLeft(mappedLeft)) {
+      expect(mappedLeft.left).toBe(4);
+    }
+    expect(mappedRight).toBe(rightValue);
+  });
+
+  test("flatMapRight() - data first", () => {
+    const leftValue: Either.Either<number, string> = Either.left(2);
+    const rightValue: Either.Either<number, string> = Either.right("value");
+
+    const flatMapFn = (value: string) => Either.right<number, string>(`${value}!`);
+
+    const mappedLeft = Either.flatMapRight(leftValue, flatMapFn);
+    const mappedRight = Either.flatMapRight(rightValue, flatMapFn);
+
+    expect(mappedLeft).toBe(leftValue);
+    expect(mappedRight._tag).toBe("right");
+    if (Either.isRight(mappedRight)) {
+      expect(mappedRight.right).toBe("value!");
+    }
+  });
+
+  test("flatMapRight() - data last", () => {
+    const leftValue: Either.Either<number, string> = Either.left(2);
+    const rightValue: Either.Either<number, string> = Either.right("value");
+
+    const flatMapFn = Either.flatMapRight((value: string) => Either.right<number, string>(`${value}!`));
+
+    const mappedLeft = flatMapFn(leftValue);
+    const mappedRight = flatMapFn(rightValue);
+
+    expect(mappedLeft).toBe(leftValue);
+    expect(mappedRight._tag).toBe("right");
+    if (Either.isRight(mappedRight)) {
+      expect(mappedRight.right).toBe("value!");
+    }
+  });
+
+  test("biFlatMap() - data first", () => {
+    const leftValue = Either.left(2);
+    const rightValue = Either.right("value");
+
+    const biFlatMapFn = {
+      onLeft: (value: number) => Either.left<number, string>(value * 2),
+      onRight: (value: string) => Either.right<number, string>(`${value}!`),
+    };
+
+    const mappedLeft = Either.biFlatMap(leftValue, biFlatMapFn);
+    const mappedRight = Either.biFlatMap(rightValue, biFlatMapFn);
+
+    expect(mappedLeft._tag).toBe("left");
+    if (Either.isLeft(mappedLeft)) {
+      expect(mappedLeft.left).toBe(4);
+    }
+    expect(mappedRight._tag).toBe("right");
+    if (Either.isRight(mappedRight)) {
+      expect(mappedRight.right).toBe("value!");
+    }
+  });
+
+  test("biFlatMap() - data last", () => {
+    const leftValue = Either.left(2);
+    const rightValue = Either.right("value");
+
+    const biFlatMapFn = Either.biFlatMap({
+      onLeft: (value: number) => Either.left<number, string>(value * 2),
+      onRight: (value: string) => Either.right<number, string>(`${value}!`),
+    });
+
+    const mappedLeft = biFlatMapFn(leftValue);
+    const mappedRight = biFlatMapFn(rightValue);
+
+    expect(mappedLeft._tag).toBe("left");
+    if (Either.isLeft(mappedLeft)) {
+      expect(mappedLeft.left).toBe(4);
+    }
+    expect(mappedRight._tag).toBe("right");
+    if (Either.isRight(mappedRight)) {
+      expect(mappedRight.right).toBe("value!");
+    }
+  });
+});
---
+++ packages/typekit/src/either/implementation.ts
@@ -0,0 +1,195 @@
+import { dual } from "~/dual";
+import { Pipe, pipeable } from "~/pipe";
+import { tagged, Tagged } from "~/tagged";
+
+export interface Left<LeftType> extends Tagged.Tagged<"left">, Pipe.Pipeable {
+  left: LeftType;
+}
+
+export interface Right<RightType> extends Tagged.Tagged<"right">, Pipe.Pipeable {
+  right: RightType;
+}
+
+export type Either<LeftType, RightType> = Left<LeftType> | Right<RightType>;
+
+export function left<LeftType = unknown, RightType = never>(value: LeftType): Either<LeftType, RightType> {
+  return pipeable(tagged({ left: value }, "left"));
+}
+
+export function right<LeftType = never, RightType = unknown>(value: RightType): Either<LeftType, RightType> {
+  return pipeable(tagged({ right: value }, "right"));
+}
+
+export function isLeft<LeftType, RightType>(either: Either<LeftType, RightType>): either is Left<LeftType> {
+  return either._tag === "left";
+}
+
+export function isRight<LeftType, RightType>(either: Either<LeftType, RightType>): either is Right<RightType> {
+  return either._tag === "right";
+}
+
+export function unwrapLeft<LeftType, RightType>(either: Either<LeftType, RightType>): LeftType {
+  if (either._tag === "right") {
+    throw new Error(`Cannot unwrapLeft right value: ${either}`);
+  }
+
+  return either.left;
+}
+
+export const unwrapLeftOr: {
+  <LeftType, RightType>(either: Either<LeftType, RightType>, defaultValue: LeftType): LeftType;
+  <LeftType>(defaultValue: LeftType): <RightType>(either: Either<LeftType, RightType>) => LeftType;
+} = dual(2, function <LeftType, RightType>(either: Either<LeftType, RightType>, defaultValue: LeftType): LeftType {
+  return either._tag === "left" ? either.left : defaultValue;
+});
+
+export function unwrapRight<LeftType, RightType>(either: Either<LeftType, RightType>): RightType {
+  if (either._tag === "left") {
+    throw new Error(`Cannot unwrapRight left value: ${either}`);
+  }
+
+  return either.right;
+}
+
+export const unwrapRightOr: {
+  <LeftType, RightType>(either: Either<LeftType, RightType>, defaultValue: RightType): RightType;
+  <RightType>(defaultValue: RightType): <LeftType>(either: Either<LeftType, RightType>) => RightType;
+} = dual(2, function <LeftType, RightType>(either: Either<LeftType, RightType>, defaultValue: RightType): RightType {
+  return either._tag === "left" ? defaultValue : either.right;
+});
+
+// export function flattenLeft<LeftType, RightType>(either: Either<Either<LeftType, RightType>, RightType>) {
+//   return either._tag === "left" ? either.left : either;
+// }
+
+export function flatten<LeftType, RightType>(
+  either: Either<LeftType, Either<LeftType, RightType>>,
+): Either<LeftType, RightType> {
+  return either._tag === "left" ? either : either.right;
+}
+
+export function flatten2<LeftType, RightType>(
+  either: Either<Either<LeftType, RightType>, Either<LeftType, RightType>>,
+): Either<LeftType, RightType> {
+  return either._tag === "left" ? either.left : either.right;
+}
+
+export const mapLeft: {
+  <LeftType1, RightType, LeftType2>(
+    either: Either<LeftType1, RightType>,
+    onLeft: (value: LeftType1) => LeftType2,
+  ): Either<LeftType2, RightType>;
+  <LeftType1, LeftType2>(
+    onLeft: (value: LeftType1) => LeftType2,
+  ): <RightType>(either: Either<LeftType1, RightType>) => Either<LeftType2, RightType>;
+} = dual(2, function <
+  LeftType1,
+  RightType,
+  LeftType2,
+>(either: Either<LeftType1, RightType>, onLeft: (value: LeftType1) => LeftType2): Either<LeftType2, RightType> {
+  return either._tag === "left" ? left(onLeft(either.left)) : either;
+});
+
+export const mapRight: {
+  <LeftType, RightType1, RightType2>(
+    either: Either<LeftType, RightType1>,
+    onRight: (value: RightType1) => RightType2,
+  ): Either<LeftType, RightType2>;
+  <RightType1, RightType2>(
+    onRight: (value: RightType1) => RightType2,
+  ): <LeftType>(either: Either<LeftType, RightType1>) => Either<LeftType, RightType2>;
+} = dual(2, function <
+  LeftType,
+  RightType1,
+  RightType2,
+>(either: Either<LeftType, RightType1>, onRight: (value: RightType1) => RightType2): Either<LeftType, RightType2> {
+  return either._tag === "left" ? either : right(onRight(either.right));
+});
+
+export const biMap: {
+  <LeftType1, RightType1, LeftType2, RightType2>(
+    either: Either<LeftType1, RightType1>,
+    func: {
+      onLeft: (value: LeftType1) => LeftType2;
+      onRight: (value: RightType1) => RightType2;
+    },
+  ): Either<LeftType2, RightType2>;
+  <LeftType1, RightType1, LeftType2, RightType2>(func: {
+    onLeft: (value: LeftType1) => LeftType2;
+    onRight: (value: RightType1) => RightType2;
+  }): (either: Either<LeftType1, RightType1>) => Either<LeftType2, RightType2>;
+} = dual(
+  2,
+  function <LeftType1, RightType1, LeftType2, RightType2>(
+    either: Either<LeftType1, RightType1>,
+    func: {
+      onLeft: (value: LeftType1) => LeftType2;
+      onRight: (value: RightType1) => RightType2;
+    },
+  ): Either<LeftType2, RightType2> {
+    return either._tag === "left" ? left(func.onLeft(either.left)) : right(func.onRight(either.right));
+  },
+);
+
+export const flatMapLeft: {
+  <LeftType1, RightType, LeftType2>(
+    either: Either<LeftType1, RightType>,
+    onLeft: (value: LeftType1) => Either<LeftType2, RightType>,
+  ): Either<LeftType2, RightType>;
+  <LeftType1, RightType, LeftType2>(
+    onLeft: (value: LeftType1) => Either<LeftType2, RightType>,
+  ): (either: Either<LeftType1, RightType>) => Either<LeftType2, RightType>;
+} = dual(2, function <
+  LeftType1,
+  RightType,
+  LeftType2,
+>(either: Either<LeftType1, RightType>, onLeft: (value: LeftType1) => Either<LeftType2, RightType>): Either<
+  LeftType2,
+  RightType
+> {
+  return either._tag === "left" ? onLeft(either.left) : either;
+});
+
+export const flatMapRight: {
+  <LeftType, RightType1, RightType2>(
+    either: Either<LeftType, RightType1>,
+    onRight: (value: RightType1) => Either<LeftType, RightType2>,
+  ): Either<LeftType, RightType2>;
+  <LeftType, RightType1, RightType2>(
+    onRight: (value: RightType1) => Either<LeftType, RightType2>,
+  ): (either: Either<LeftType, RightType1>) => Either<LeftType, RightType2>;
+} = dual(2, function <
+  LeftType,
+  RightType1,
+  RightType2,
+>(either: Either<LeftType, RightType1>, onRight: (value: RightType1) => Either<LeftType, RightType2>): Either<
+  LeftType,
+  RightType2
+> {
+  return either._tag === "left" ? either : onRight(either.right);
+});
+
+export const biFlatMap: {
+  <LeftType1, RightType1, LeftType2, RightType2>(
+    either: Either<LeftType1, RightType1>,
+    func: {
+      onLeft: (value: LeftType1) => Either<LeftType2, RightType2>;
+      onRight: (value: RightType1) => Either<LeftType2, RightType2>;
+    },
+  ): Either<LeftType2, RightType2>;
+  <LeftType1, RightType1, LeftType2, RightType2>(func: {
+    onLeft: (value: LeftType1) => Either<LeftType2, RightType2>;
+    onRight: (value: RightType1) => Either<LeftType2, RightType2>;
+  }): (either: Either<LeftType1, RightType1>) => Either<LeftType2, RightType2>;
+} = dual(
+  2,
+  function <LeftType1, RightType1, LeftType2, RightType2>(
+    either: Either<LeftType1, RightType1>,
+    func: {
+      onLeft: (value: LeftType1) => Either<LeftType2, RightType2>;
+      onRight: (value: RightType1) => Either<LeftType2, RightType2>;
+    },
+  ): Either<LeftType2, RightType2> {
+    return either._tag === "left" ? func.onLeft(either.left) : func.onRight(either.right);
+  },
+);
