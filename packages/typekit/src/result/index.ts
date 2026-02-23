fc21d222ae3b95d39ec2befdb658da0f0117e265
pekochan069 <pekochan069@gmail.com>
2026-02-23 23:55:39 +0900

    feat: remove individual function export

--- packages/typekit/src/result/index.ts
+++ packages/typekit/src/result/index.ts
@@ -1,18 +1,1 @@
 export * as Result from "./implementation";
-export {
-  ok,
-  err,
-  isOk,
-  isErr,
-  unwrap,
-  unwrapOr,
-  unwrapErr,
-  unwrapErrOr,
-  flatten,
-  map,
-  mapErr,
-  biMap,
-  flatMap,
-  flatMapErr,
-  biFlatMap,
-} from "./implementation";
