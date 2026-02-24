15dbdd8c3b8a811d36493debc444a0cce2a0cc6e
pekochan069 <97679910+pekochan069@users.noreply.github.com>
2026-02-24 12:41:05 +0900

    feat: re export individual functions

--- packages/typekit/src/either/index.ts
+++ packages/typekit/src/either/index.ts
@@ -1,1 +1,2 @@
 export * as Either from "./implementation";
+export { left, right, isLeft, isRight, unwrapLeft, unwrapLeftOr, unwrapRight, unwrapRightOr, flatten, flatten2, mapLeft, mapRight, biMap, flatMapLeft, flatMapRight, biFlatMap } from "./implementation";
