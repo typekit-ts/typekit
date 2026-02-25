e15304a2c15ac5f9691c66967a68f9e5203fb619
pekochan069 <pekochan069@gmail.com>
2026-02-26 00:52:27 +0900

    feat: add 3 arity variant

--- packages/typekit/src/dual/implementation.ts
+++ packages/typekit/src/dual/implementation.ts
@@ -1,24 +1,36 @@
-type AnyFn = (...args: any[]) => any;
+import type { AnyFunction } from "~/types";
 
-export function dual<DataFirst extends AnyFn, DataLast extends AnyFn>(
+export function dual<DataFirst extends AnyFunction, DataLast extends AnyFunction>(
   arity: number,
-  body: AnyFn,
+  body: DataFirst,
 ): DataFirst & DataLast {
-  if (arity === 2) {
-    return function (a: any, b: any) {
-      if (arguments.length === 2) {
-        return body(a, b);
-      }
+  switch (arity) {
+    case 2: {
+      return function (a: any, b: any) {
+        if (arguments.length === 2) {
+          return body(a, b);
+        }
 
-      return (b: any) => body(b, a);
-    } as any;
-  }
-
-  return function (...args: any[]) {
-    if (args.length >= arity) {
-      return body(...args);
+        return (b: any) => body(b, a);
+      } as any;
     }
+    case 3: {
+      return function (a: any, b: any, c: any) {
+        if (arguments.length === 3) {
+          return body(a, b, c);
+        }
 
-    return (a: any) => body(a, ...args);
-  } as any;
+        return (c: any) => body(c, a, b);
+      } as any;
+    }
+    default: {
+      return function (...args: any[]) {
+        if (args.length >= arity) {
+          return body(...args);
+        }
+
+        return (a: any) => body(a, ...args);
+      } as any;
+    }
+  }
 }
