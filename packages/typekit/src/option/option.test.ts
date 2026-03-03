89490322e1cac63a4ed309fc678280772698bf98
pekochan069 <pekochan069@gmail.com>
2026-03-04 00:47:45 +0900

    feat: add test cases for added Option api

--- packages/typekit/src/option/option.test.ts
+++ packages/typekit/src/option/option.test.ts
@@ -15,6 +15,22 @@ describe("option", () => {
     expect(noneValue._tag).toBe("none");
   });
 
+  test("isSome()", () => {
+    const someValue = Option.some(1);
+    const noneValue = Option.none<number>();
+
+    expect(Option.isSome(someValue)).toBe(true);
+    expect(Option.isSome(noneValue)).toBe(false);
+  });
+
+  test("isNone()", () => {
+    const someValue = Option.some(1);
+    const noneValue = Option.none<number>();
+
+    expect(Option.isNone(someValue)).toBe(false);
+    expect(Option.isNone(noneValue)).toBe(true);
+  });
+
   test("ap() - data first", () => {
     const someValue = Option.some(1);
     const noneValue = Option.none<number>();
@@ -112,5 +128,76 @@ describe("option", () => {
       expect(mappedSome.value).toBe(4);
     }
     expect(mappedNone).toBe(noneValue);
+  });
+
+  test("flatten()", () => {
+    const nestedSomeValue = Option.some(Option.some(1));
+    const nestedNoneValue = Option.some(Option.none());
+    const noneValue = Option.none<Option.Option<Option.Option<number>>>();
+
+    const flattenedNestedSome = Option.flatten(nestedSomeValue);
+    const flattenedNestedNone = Option.flatten(nestedNoneValue);
+    const flattenedNoneValue = Option.flatten(noneValue);
+
+    expect(flattenedNestedSome._tag).toBe("some");
+    if (Option.isSome(flattenedNestedSome)) {
+      expect(flattenedNestedSome.value).toBe(1);
+    }
+
+    expect(flattenedNestedNone._tag).toBe("none");
+
+    expect(flattenedNoneValue._tag).toBe("none");
+  });
+
+  test("match() - data first", () => {
+    const someValue = Option.some(1);
+    const noneValue = Option.none<number>();
+
+    const matchFn = {
+      isSome: (value: number) => value,
+      isNone: () => 2,
+    };
+
+    const matchedSome = Option.match(someValue, matchFn);
+    const matchedNone = Option.match(noneValue, matchFn);
+
+    expect(matchedSome).toBe(1);
+    expect(matchedNone).toBe(2);
+  });
+
+  test("match() - data last", () => {
+    const someValue = Option.some(1);
+    const noneValue = Option.none<number>();
+
+    const matchFn = {
+      isSome: (value: number) => value,
+      isNone: () => 2,
+    };
+
+    const matchedSome = Option.match(matchFn)(someValue);
+    const matchedNone = Option.match(matchFn)(noneValue);
+
+    expect(matchedSome).toBe(1);
+    expect(matchedNone).toBe(2);
+  });
+
+  test("pipe()", () => {
+    const someValue = Option.some(1).pipe(
+      Option.map((value) => value + 2),
+      Option.flatMap((value) => Option.some(value * 2)),
+      Option.map((value) => value + 1),
+    );
+    const noneValue = Option.none<number>().pipe(
+      Option.map((value) => value + 2),
+      Option.flatMap((value) => Option.some(value * 2)),
+      Option.map((value) => value + 1),
+    );
+
+    expect(someValue._tag).toBe("some");
+    if (Option.isSome(someValue)) {
+      expect(someValue.value).toBe(7);
+    }
+
+    expect(noneValue._tag).toBe("none");
   });
 });
